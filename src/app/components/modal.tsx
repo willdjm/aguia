a534aa0349996c68d9ecf63dbbc464037140d5a4
CJJ <cassio.jjesus@gmail.com>
2023-10-25 09:56:56 -0300

    chore

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -1,54 +1,65 @@
 import { useState } from "react";
 
-export default function Modal() {
-const [showModal, setShowModal] = useState(false);
-return (
-<>
-{/*BOTÃO ABRIR MODAL*/}
-<button className="block text-white bg-blue-700 hover:bg-blue-500 font-medium rounded-lg text-sm px-2 py-2.5 text-center"
-type="button"
-onClick={() => setShowModal(true)}>
-Confirmar Pagamento
-</button>
-{showModal ? (
-<>
-  <div
-    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-  >
-    {/*BOX MODAL*/}
-    <div className="relative w-full  mx-auto max-w-2xl">
-      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-        {/*HEADER MODAL*/}
-        <div className="flex items-center justify-center p-5 border-b border-solid ">
-          <img src="/logo.png" alt="" className="w-20 h-24" />
-        </div>
+export default function Modal({ id, name }: any) {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      {/*BOTÃO ABRIR MODAL*/}
+      <button
+        className="block text-white bg-blue-700 hover:bg-blue-500 font-medium rounded-lg text-sm px-2 py-2.5 text-center"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Confirmar Pagamento
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            {/*BOX MODAL*/}
+            <div className="relative w-full  mx-auto max-w-2xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*HEADER MODAL*/}
+                <div className="flex items-center justify-center p-5 border-b border-solid ">
+                  <img src="/logo.png" alt="" className="w-20 h-24" />
+                </div>
 
-        {/*BODY MODAL*/}
-        <div className="relative p-6 flex-auto">
-          <p className="text-black text-2xl">Deseja confirmar o pagamento?</p>
-        </div>
+                {/*BODY MODAL*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="text-black text-2xl">
+                    Deseja confirmar o pagamento?
+                  </p>
+                  <p>Id do inscrito: {id}</p>
+                  <p>Nome do incrito: {name}</p>
+                </div>
 
-        {/*FOOTER MODAL / BOTÕES */}
-        <div className="flex items-center justify-center p-5 border-b border-solid gap-6">
+                {/*FOOTER MODAL / BOTÕES */}
+                <div className="flex items-center justify-center p-5 border-b border-solid gap-6">
+                  {/*BOTÃO CANCELAR*/}
+                  <button
+                    type="button"
+                    className="text-gray-500 bg-white rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-white hover:bg-red-600 focus:z-10"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancelar
+                  </button>
 
-          {/*BOTÃO CANCELAR*/}
-          <button type="button" className="text-gray-500 bg-white rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-white hover:bg-red-600 focus:z-10"
-            onClick={() => setShowModal(false)}>Cancelar
-          </button>
+                  {/*BOTÃO CONFIRMAR*/}
+                  <button
+                    type="button"
+                    className="text-white bg-emerald-500 hover:bg-emerald-400 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Confirmar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
 
-          {/*BOTÃO CONFIRMAR*/}
-          <button type="button" className="text-white bg-emerald-500 hover:bg-emerald-400 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-            onClick={() => setShowModal(false)}>Confirmar
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  {/*FUNDO MODAL*/}
-  <div className="opacity-80 fixed inset-0 z-40 bg-slate-200"></div>
-</>
-) : null}
-</>
-);
+          {/*FUNDO MODAL*/}
+          <div className="opacity-80 fixed inset-0 z-40 bg-slate-200"></div>
+        </>
+      ) : null}
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,116 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Modal from "./components/modal";
 import { useFetch } from "./hooks/useFetch";
 
 type Repository = {
-id: number;
-name: string;
-phone: string;
-email: string
-contact_name: string
-contact_phone: string
-tshirt: string
-vise: 'true' | 'false'
-
-}
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+  contact_name: string;
+  contact_phone: string;
+  tshirt: string;
+  vise: "true" | "false";
+};
 
 export default function Home() {
+  const { data: repositories } = useFetch<Repository[]>("goal-2023/list");
 
-const { data: repositories } =
-useFetch<Repository[]>('goal-2023/list')
-
-return (
-
-<div className="flex items-center justify-center w-full">
-<div className="grid items-center justify-center max-w-screen-2xl w-full py-16">
-<h1 className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 text-5xl font-extrabold">Cadastro Alunos</h1>
-<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-auto">
-  <thead className="text-sm font-bold text-gray-700 uppercase bg-gray-200">
-    <tr className="border-b">
-      <th className="px-2 py-6 text-sm text-center">Id</th>
-      <th scope="col" className="px-2 py-3 text-center">Nome</th>
-      <th scope="col" className="px-2 py-3 text-center">Telefone</th>
-      <th scope="col" className="px-2 py-3 text-center">E-mail</th>
-      <th scope="col" className="px-2 py-3 text-center">Contato</th>
-      <th scope="col" className="px-2 py-3 text-center">Tel. Contato</th>
-      <th scope="col" className="px-2 py-3 text-center">Camiseta</th>
-      <th scope="col" className="px-2 py-3 text-center">Viseira / Boné</th>
-      <th scope="col" className="px-2 py-3 text-center">Valor</th>
-      <th scope="col" className="px-2 py-3 text-center">Pagamento</th>
-    </tr>
-  </thead>
-  {repositories?.map(list => {
-
-    {/*FUNÇÃO VISEIRA*/ }
-    function viseira() {
-      if (list.vise && 'true') {
-        return <p className="text-sm font-semibold text-blue-500">Sim</p>;
-      } else {
-        return <p className="text-sm font-semibold text-red-500">Nao</p>;
-      }
-    }
-    return (
-
-      <tbody key={list.id}>
-        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-          <td className="whitespace-nowrap px-2 py-4 text-sm text-center font-bold text-red-500">{list.id}</td>
-          <td className="whitespace-nowrap px-2 py-4 font-semibold">{list.name}</td>
-          <td className="whitespace-nowrap px-2 py-4">{list.phone}</td>
-          <td className="whitespace-nowrap px-2 py-4 text-center">{list.email}</td>
-          <td className="whitespace-nowrap px-2 py-4">{list.contact_name}</td>
-          <td className="whitespace-nowrap px-2 py-4">{list.contact_phone}</td>
-          <td className="whitespace-nowrap px-2 py-4 text-center">{list.tshirt}</td>
-          <td className="whitespace-nowrap px-2 py-4 text-center">{viseira()}</td>
-          <td className="whitespace-nowrap px-2 py-4 text-emerald-500">R$ 120</td>
-          <td className="whitespace-nowrap px-2 py-4 text-center">
-            <button><Modal /></button>
-          </td>
-        </tr>
-      </tbody>
-    );
-  })}
-</table>
-</div>
-</div>
-);
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div className="grid items-center justify-center max-w-screen-2xl w-full py-16">
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 text-5xl font-extrabold">
+          Cadastro Alunos
+        </h1>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-auto">
+          <thead className="text-sm font-bold text-gray-700 uppercase bg-gray-200">
+            <tr className="border-b">
+              <th className="px-2 py-6 text-sm text-center">Id</th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Nome
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Telefone
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                E-mail
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Contato
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Tel. Contato
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Camiseta
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Viseira / Boné
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Valor
+              </th>
+              <th scope="col" className="px-2 py-3 text-center">
+                Pagamento
+              </th>
+            </tr>
+          </thead>
+          {repositories?.map((list) => {
+            {
+              /*FUNÇÃO VISEIRA*/
+            }
+            function viseira() {
+              if (list.vise && "true") {
+                return (
+                  <p className="text-sm font-semibold text-blue-500">Sim</p>
+                );
+              } else {
+                return (
+                  <p className="text-sm font-semibold text-red-500">Nao</p>
+                );
+              }
+            }
+            return (
+              <tbody key={list.id}>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="whitespace-nowrap px-2 py-4 text-sm text-center font-bold text-red-500">
+                    {list.id}
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4 font-semibold">
+                    {list.name}
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4">{list.phone}</td>
+                  <td className="whitespace-nowrap px-2 py-4 text-center">
+                    {list.email}
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4">
+                    {list.contact_name}
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4">
+                    {list.contact_phone}
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4 text-center">
+                    {list.tshirt}
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4 text-center">
+                    {viseira()}
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4 text-emerald-500">
+                    R$ 120
+                  </td>
+                  <td className="whitespace-nowrap px-2 py-4 text-center">
+                    <button>
+                      <Modal id={list.id} name={list.name} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
+    </div>
+  );
 }
